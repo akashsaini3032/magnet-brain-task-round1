df3555c7b75e0def630f0ccbcd881c88643949ce
akashsaini3032 <akashsainira@gmail.com>
2025-10-30 22:40:44 +0530

    server side work and client side setup

---
+++ server/routes/adminRoute.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const route = express.Router();
+const AdminController= require("../controllers/AdminController"); 
+route.post("/logincheck", AdminController.adminLogin);
+route.post("/usercreation", AdminController.createUser);
+route.get("/showuserdata", AdminController.showUserData);
+route.post("/assigntask", AdminController.assignTask);
+route.get("/taskdetail", AdminController.taskDetail);
+route.get("/changetaskstatus", AdminController.changeTaskStatus);
+
+
+module.exports=route;
